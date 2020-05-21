b771cc5ff80cc2034f371927e179d70f9e5cc845
Troy <troykirin@gmail.com>
2020-05-20 17:38:30 -0700

    profile page go back to previous page

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-export default class ProfileScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Welcome to Profile Screen!</Text>
-      </View>
-    );
-  }
+function ProfileScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Welcome to Profile Screen!</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()}></Button>
+    </View>
+  );
 }
+
+export default ProfileScreen;
