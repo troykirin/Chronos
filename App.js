565fa58083a85706510e86d8480af9f0aa25705b
Troy <troykirin@gmail.com>
2020-05-26 17:44:28 -0700

    move firerbase to login, screen import

--- App.js
+++ App.js
@@ -4,20 +4,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from "react-native";
 
 //Screens
+import LoadingScreen from "./src/screens/Load";
 import HomeScreen from "./src/screens/Home";
-import ProfileScreen from "./src/screens/Profile";
 import LoginScreen from "./src/screens/Login";
+import ProfileScreen from "./src/screens/Profile";
+// import DashboardScreen from "./src/screens/Dashboard";
 
+// firebase
+import firebase from "firebase";
+firebase.initializeApp(firebaseConfig);
+import { firebaseConfig } from "./config";
 // create app navigator
-
 const Stack = createStackNavigator();
 
 function RootStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Loading" component={LoadingScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
+      {/* <Stack.Screen name="Dashboard" component={DashboardScreen} /> */}
     </Stack.Navigator>
   );
 }
