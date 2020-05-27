ae0673064eb31e93614d405b93ec531bc407f10f
Troy <troykirin@gmail.com>
2020-05-26 17:45:20 -0700

    profilie pic & style for border

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,13 +1,69 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image, StyleSheet } from "react-native";
 
 function ProfileScreen({ navigation }) {
   return (
     <View>
       <Text>Welcome to Profile Screen!</Text>
       <Button title="Go Back" onPress={() => navigation.goBack()}></Button>
+      <Image
+        style={styles.image}
+        source={require("../../assets/profile_pic.jpg")}
+      />
     </View>
   );
 }
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: "center",
+    color: "#656565",
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: "center",
+  },
+  flowRight: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "stretch",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center",
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#48BBEC",
+    borderColor: "#48BBEC",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+    justifyContent: "center",
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flex: 4,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "#48BBEC",
+    borderRadius: 8,
+    color: "#48BBEC",
+  },
+  image: {
+    width: 200,
+    height: 220,
+    alignSelf: "center",
+  },
+});
