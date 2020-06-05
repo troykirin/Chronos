1bb41b607d243a11215f6d4e0279ed865c149469
Troy <troykirin@gmail.com>
2020-06-05 10:07:23 -0700

    implement simple counter

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button, StyleSheet, useState } from "react-native";
 
 class HomeScreen extends Component {
+  // DEV Note: When component instanciated, constructor is ran.
   constructor(props) {
     super(props);
+    this.state = { count: 0 };
   }
+
+  // Function to decrease state of 'count' by 1
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
   render() {
     return (
@@ -14,8 +20,17 @@ class HomeScreen extends Component {
           title="Go to Profile"
           onPress={() => this.props.navigation.navigate("Profile")}
         ></Button>
-        <Text>You are at the homescreen.</Text>
-        <Text>LoginState is: </Text>
+
+        <Text>Welcome Home! </Text>
+
+        <Button
+          title="Increment"
+          onPress={() => this.setState({ count: this.state.count + 1 })}
+        ></Button>
+
+        <Button title="Decrease" onPress={() => this.decrement()}></Button>
+
+        <Text>Current Count: {this.state.count}</Text>
       </View>
     );
   }
